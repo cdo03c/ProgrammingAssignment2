ad843bdef4e660b10a10fb5dececadf0c10e4c01
cdo03c <cdo03c@gmail.com>
2014-10-25 13:49:45 +0100

    This file has the completed functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions makeCacheMatrix caches an inverse of a given matrix,
+## while cacheSolve checks to see if there is a cache of the given matrix
+## to send before running the calculation.
 
-## Write a short comment describing this function
+
+## This function checks to see if the given matrix x is an n by n 
+## matrix then calculates the inverse matrix and caches the results.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <-NULL
+  if (nrow(x) == ncol(x)){
+    m <<- solve(x)
+  } else {
+    print("This is not an n by n matrix.")
+  }
+  z <<- x
 }
 
-
-## Write a short comment describing this function
+## This function checks to see if that the original matrix used in 
+## makeCacheMatrix() and the current matrix are the same.  Then it checks 
+## if there is a cached inverse matrix of the given matrix.  If so, it 
+## returns the cached inverse.  Otherwise its runs the calculation to cache
+## a new matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  if(identical(x, z)){
+    if (!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    } else{
+      print("The inverse of the matrix was not cached, but here is the inverse")
+      q <- makeCacheMatrix(x)
+      q
+      } 
+    } else {
+      print("This is not the same matrix that was used to cache the inverse; first use makeCacheMAtrix()")
+    }
+  
 }
